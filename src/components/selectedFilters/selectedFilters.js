dc28200266f184bbe5e76002782e879895417750
Fabio Bier <fab.bier@gmail.com>
2017-11-10 15:01:49 +0100

    Basic pill-button and filter styling.

--- src/components/selectedFilters/selectedFilters.js
+++ src/components/selectedFilters/selectedFilters.js
@@ -5,15 +5,15 @@ export default class Wrapper extends Component {
     constructor(props, context) {
         super(props, context);
     }
-    
+
     render() {
         return (
             <div className="filters">
                 {Object.keys(this.props.filters).map((keyName, index) => {
-                    return ( 
+                    return (
                         <div key={index}>
-                            {keyName} {this.props.filters[keyName] || 'nothing selected'}
-                            <input style={ {"border": "1px solid black"}} type="text" onChange={(e) => { 
+                            <button className="pill-button">{keyName} {this.props.filters[keyName] || 'nothing selected'}</button>
+                            <input style={ {"border": "1px solid black"}} type="text" onChange={(e) => {
                                 this.props.changeFilter(this.props.day, keyName, e.target.value)
                             }} />
                         </div>
