9b916a8684d868955a203cbdfe496d2027d55874
David den Toom <daviddentoom@gmail.com>
2017-11-10 13:49:17 +0100

    change days

--- src/components/day-picker/day-picker.js
+++ src/components/day-picker/day-picker.js
@@ -3,18 +3,14 @@ import { IndexLink, Link } from 'react-router'
 
 export default class DayPicker extends Component {
   render() {
+    const days = ['zaterdag', 'zondag', 'maandag', 'dinsdag', 'woensdag', 'donderdag', 'vrijdag']
     return (
       <div className="daypicker">
-        {/*<IndexLink to='/' activeClassName='page-layout__nav-item--active'>Home</IndexLink>*/}
-        {/*{' · '}*/}
-        {/*<Link to='/counter' activeClassName='page-layout__nav-item--active'>Counter</Link>*/}
-        <Link to='/meal/zaterdag' activeClassName='page-layout__nav-item--active'>Za</Link>
-        <Link to='/meal/zondag' activeClassName='page-layout__nav-item--active'>Zo</Link>
-        <Link to='/meal/maandag' activeClassName='page-layout__nav-item--active'>Ma</Link>
-        <Link to='/meal/dinsdag' activeClassName='page-layout__nav-item--active'>Di</Link>
-        <Link to='/meal/woensdag' activeClassName='page-layout__nav-item--active'>Wo</Link>
-        <Link to='/meal/donderdag' activeClassName='page-layout__nav-item--active'>Do</Link>
-        <Link to='/meal/vrijdag' activeClassName='page-layout__nav-item--active'>Vr</Link>
+        {days.map((day, i) => {
+          return (
+            <Link key={i} to={`/meal/${day}`} className={`${this.props.selectedMeals[day] ? 'has--meal' : ''}`}activeClassName='page-layout__nav-item--active'>{day.substring(0, 1).toUpperCase() + day.substring(1, 2)}</Link>
+          )
+        })}
       </div>
     )
   }
