39f5db1d15c54d0ddc2c0c81156d552f3f64568e
David den Toom <daviddentoom@gmail.com>
2017-11-10 16:57:34 +0100

    ultracommit

--- src/routes/List/components/ListView.js
+++ src/routes/List/components/ListView.js
@@ -1,10 +1,13 @@
 import React from 'react'
 import { browserHistory } from 'react-router'
+import List from './../containers/List'
 
 export const ListView = (props) => (
   <div>
     <button onClick={() => browserHistory.push('/meal/zaterdag')}>Back to shop</button>
     <button onClick={() => browserHistory.push('/thank-you')}>Accept</button>
+
+    <List />
   </div>
 )
 
---
+++ src/routes/List/containers/List.js
@@ -0,0 +1,37 @@
+import { connect } from 'react-redux'
+import React from 'react'
+
+export const ListContainer = (props) => (
+    <div className="selected-meals">
+        {Object.keys(props.selectedMeals).map((day, i) => {
+            return (
+                <ul key={i}>
+                    <li>
+                        <h2>{props.selectedMeals[day].name}</h2>
+                        {props.selectedMeals[day].ingredients.map((ingredient, j) => {
+                            return <div key={j}>{ingredient.name}</div>
+                        })}
+                    </li>
+                </ul>
+            )
+        })}
+    </div>
+  )
+
+const mapStateToProps = (state) => {
+    const selectedMeals = []
+
+    Object.keys(state.selectedMeals).map(day => {
+        if (typeof state.selectedMeals[day] !== 'string') {
+            selectedMeals.push(state.selectedMeals[day])
+        }
+    })
+
+    return {
+        selectedMeals
+    }
+}
+
+
+
+export default connect(mapStateToProps)(ListContainer)
