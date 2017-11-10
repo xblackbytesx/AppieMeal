3917447acd57f753ca770c1a6b27ae2e8b356432
David den Toom <daviddentoom@gmail.com>
2017-11-10 09:44:16 +0100

    meals

--- src/store/meals.js
+++ src/store/meals.js
@@ -1,12 +1,10 @@
+import meals from './meals.json'
+
 // ------------------------------------
 // Reducer
 // ------------------------------------
-const initialState = [
-    { name: 'meal 1', price: 10 },
-    { name: 'meal 2', price: 5  },
-    { name: 'meal 3', price: 7  },
-    { name: 'meal 4', price: 8  }
-]
+const initialState = meals.meals
+
 export default function mealReducer (state = initialState, action) {
   return state
 }
