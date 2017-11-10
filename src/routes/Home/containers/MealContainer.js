ba853b9a982c91474c41b8ee818cace645303f97
David den Toom <daviddentoom@gmail.com>
2017-11-10 10:13:57 +0100

    select meal per da

--- src/routes/Home/containers/MealContainer.js
+++ src/routes/Home/containers/MealContainer.js
@@ -1,23 +1,15 @@
 import { connect } from 'react-redux'
 
+import { selectMeal } from './../../../store/selectedMeals'
+
 import Meals from '../../../components/meals/meals'
 
 const mapStateToProps = (state) => ({
   meals : state.meals
 })
 
-/*  Note: mapStateToProps is where you should use `reselect` to create selectors, ie:
+const mapDispatchToProps = {
+  selectMeal,
+}
 
-    import { createSelector } from 'reselect'
-    const counter = (state) => state.counter
-    const tripleCount = createSelector(counter, (count) => count * 3)
-    const mapStateToProps = (state) => ({
-      counter: tripleCount(state)
-    })
-
-    Selectors can compute derived data, allowing Redux to store the minimal possible state.
-    Selectors are efficient. A selector is not recomputed unless one of its arguments change.
-    Selectors are composable. They can be used as input to other selectors.
-    https://github.com/reactjs/reselect    */
-
-export default connect(mapStateToProps)(Meals)
+export default connect(mapStateToProps, mapDispatchToProps)(Meals)
